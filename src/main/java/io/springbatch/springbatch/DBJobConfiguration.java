cc97b157c8a64f55452ef54b24e0aca20320e705
psjw <dlrudtn108@gmail.com>
2022-05-05 11:59:52 +0900

    20220505

--- src/main/java/io/springbatch/springbatch/DBJobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/DBJobConfiguration.java
@@ -14,28 +14,26 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class HelloJobConfiguration {
+public class DBJobConfiguration {
+
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(helloStep1())
-                .next(helloStep2())
+    public Job job(){
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
-    public Step helloStep1() {
-        return stepBuilderFactory.get("helloStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("===========================");
-                        System.out.println(">> Hello Spring Batch!!!");
-                        System.out.println("===========================");
-                        //RepeatStatus.FINISHED or null : 한번 실행 후 종료
+                        System.out.println("step1 was executed.");
                         return RepeatStatus.FINISHED;
                     }
                 })
@@ -43,19 +41,15 @@ public class HelloJobConfiguration {
     }
 
     @Bean
-    public Step helloStep2() {
-        return stepBuilderFactory.get("helloStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("===========================");
-                        System.out.println(">> Step2 was executed!!!");
-                        System.out.println("===========================");
-                        //RepeatStatus.FINISHED or null : 한번 실행 후 종료
+                        System.out.println("step2 was executed.");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
     }
 }
-
