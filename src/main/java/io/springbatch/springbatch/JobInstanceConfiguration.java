110ebd29c340fe88ab436063d58b4c6e0d048435
psjw <dlrudtn108@gmail.com>
2022-06-07 11:08:13 +0900

    jobparameter

---
+++ src/main/java/io/springbatch/springbatch/JobInstanceConfiguration.java
@@ -0,0 +1,43 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+
+//@Configuration
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job(){
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+//    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+//    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((stepContribution, chunkContext) -> {
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+}
