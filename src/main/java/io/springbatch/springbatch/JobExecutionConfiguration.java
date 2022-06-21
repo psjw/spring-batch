bf52cffadcdc974ae72bb0186181e1907371829a
psjw <dlrudtn108@gmail.com>
2022-06-21 09:59:17 +0900

    JobExecution

--- src/main/java/io/springbatch/springbatch/JobExecutionConfiguration.java
+++ src/main/java/io/springbatch/springbatch/JobExecutionConfiguration.java
@@ -2,7 +2,6 @@ package io.springbatch.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -10,51 +9,41 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 /**
  * packageName : io.springbatch.springbatch
- * fileName : JobParameterConfiguration
+ * fileName : JobExecutionConfiguration
  * author : psjw
  */
-
-
 @Configuration
 @RequiredArgsConstructor
-public class JobParameterConfiguration {
+public class JobExecutionConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job batchJob(){
-        return  jobBuilderFactory.get("Job")
+    public Job batchJob() {
+        return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
                 .build();
     }
 
-
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(((stepContribution, chunkContext) -> {
-                    JobParameters jobParameters = stepContribution.getStepExecution().getJobExecution().getJobParameters();
-                    System.out.println("####step1###");
-                    jobParameters.getString("name");
-                    jobParameters.getLong("seq");
-                    jobParameters.getDate("date");
-                    jobParameters.getDouble("age");
-
-                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("###step1###");
                     return RepeatStatus.FINISHED;
-                })).build();
+                }).build();
     }
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(((stepContribution, chunkContext) -> {
-                    System.out.println("####step2###");
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("###step2###");
+//                    throw new RuntimeException("step2 has failed");
                     return RepeatStatus.FINISHED;
-                })).build();
+                }).build();
     }
 }
