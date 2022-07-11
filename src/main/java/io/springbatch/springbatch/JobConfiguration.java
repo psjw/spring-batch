7ebc12f89fbb929f9ae8ac34364320104b5a7dd7
psjw <dlrudtn108@gmail.com>
2022-07-11 21:52:16 +0900

    BatchInitialization

---
+++ src/main/java/io/springbatch/springbatch/JobConfiguration.java
@@ -0,0 +1,77 @@
+package io.springbatch.springbatch;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob1(){
+        return this.jobBuilderFactory.get("batchJob1")
+                .incrementer(new RunIdIncrementer())
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Job batchJob2(){
+        return this.jobBuilderFactory.get("batchJob2")
+                .incrementer(new RunIdIncrementer())
+                .start(step3())
+                .next(step4())
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step3 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step4 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
--- src/main/java/io/springbatch/springbatch/Member.java
+++
@@ -1,9 +0,0 @@
-package io.springbatch.springbatch;
-
-import lombok.Data;
-
-@Data
-public class Member {
-    private String id;
-
-}
